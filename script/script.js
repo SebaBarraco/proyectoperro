63a1a3dbda94609d68d4caf5d6f0aee1bdced6ef
SebaBarraco <sebabarraco@gmail.com>
2023-07-10 20:42:56 -0300

    primer commit

---
+++ script/script.js
@@ -0,0 +1,286 @@
+// evento captura click homero
+
+document.getElementById("homero").addEventListener("click", verHomero);
+
+function verHomero() {
+  elementoHomero = document.getElementById("info-homero");
+  elementoHomero.style.display = 'inline';
+  homeroDatos = document.getElementById("homero");
+  homeroDatos.style.background = '#def2f1';
+  
+  elementoLuna = document.getElementById("info-luna");
+  elementoLuna.style.display = 'none';
+  lunaDatos = document.getElementById("luna");
+  lunaDatos.style.background = 'none';
+
+  elementoLobito = document.getElementById("info-lobito");
+  elementoLobito.style.display = 'none';
+  lobitoDatos = document.getElementById("lobito");
+  lobitoDatos.style.background = 'none';
+  
+  elementoYunica = document.getElementById("info-yunica");
+  elementoYunica.style.display = 'none';
+  yunicaDatos = document.getElementById("yunica");
+  yunicaDatos.style.background = 'none'; 
+
+  elementoLola = document.getElementById("info-lola");
+  elementoLola.style.display = 'none';
+  lolaDatos = document.getElementById("lola");
+  lolaDatos.style.background = 'none'; 
+
+  elementoOli = document.getElementById("info-oli");
+  elementoOli.style.display = 'none';
+  oliDatos = document.getElementById("oli");
+  oliDatos.style.background = 'none'; 
+
+  elementoPelu= document.getElementById("info-pelu");
+  elementoPelu.style.display = 'none';
+  peluDatos = document.getElementById("pelu");
+  peluDatos.style.background = 'none'; 
+}
+
+// evento captura click luna
+
+document.getElementById("luna").addEventListener("click", verLuna);
+
+function verLuna() {
+  elementoHomero = document.getElementById("info-homero");
+  elementoHomero.style.display = 'none';
+  homeroDatos = document.getElementById("homero");
+  homeroDatos.style.background = 'none';
+  
+  elementoLuna = document.getElementById("info-luna");
+  elementoLuna.style.display = 'inline';
+  lunaDatos = document.getElementById("luna");
+  lunaDatos.style.background = '#def2f1';
+
+  elementoLobito = document.getElementById("info-lobito");
+  elementoLobito.style.display = 'none';
+  lobitoDatos = document.getElementById("lobito");
+  lobitoDatos.style.background = 'none';
+  
+  elementoYunica = document.getElementById("info-yunica");
+  elementoYunica.style.display = 'none';
+  yunicaDatos = document.getElementById("yunica");
+  yunicaDatos.style.background = 'none'; 
+
+  elementoLola = document.getElementById("info-lola");
+  elementoLola.style.display = 'none';
+  lolaDatos = document.getElementById("lola");
+  lolaDatos.style.background = 'none'; 
+
+  elementoOli = document.getElementById("info-oli");
+  elementoOli.style.display = 'none';
+  oliDatos = document.getElementById("oli");
+  oliDatos.style.background = 'none'; 
+
+  elementoPelu= document.getElementById("info-pelu");
+  elementoPelu.style.display = 'none';
+  peluDatos = document.getElementById("pelu");
+  peluDatos.style.background = 'none'; 
+}
+
+// evento captura click lobito
+
+document.getElementById("lobito").addEventListener("click", verLobito);
+
+function verLobito() {
+  elementoHomero = document.getElementById("info-homero");
+  elementoHomero.style.display = 'none';
+  homeroDatos = document.getElementById("homero");
+  homeroDatos.style.background = 'none';
+  
+  elementoLuna = document.getElementById("info-luna");
+  elementoLuna.style.display = 'none';
+  lunaDatos = document.getElementById("luna");
+  lunaDatos.style.background = 'none';
+
+  elementoLobito = document.getElementById("info-lobito");
+  elementoLobito.style.display = 'inline';
+  lobitoDatos = document.getElementById("lobito");
+  lobitoDatos.style.background = '#def2f1';
+  
+  elementoYunica = document.getElementById("info-yunica");
+  elementoYunica.style.display = 'none';
+  yunicaDatos = document.getElementById("yunica");
+  yunicaDatos.style.background = 'none'; 
+
+  elementoLola = document.getElementById("info-lola");
+  elementoLola.style.display = 'none';
+  lolaDatos = document.getElementById("lola");
+  lolaDatos.style.background = 'none'; 
+
+  elementoOli = document.getElementById("info-oli");
+  elementoOli.style.display = 'none';
+  oliDatos = document.getElementById("oli");
+  oliDatos.style.background = 'none'; 
+
+  elementoPelu= document.getElementById("info-pelu");
+  elementoPelu.style.display = 'none';
+  peluDatos = document.getElementById("pelu");
+  peluDatos.style.background = 'none'; 
+}
+
+// evento captura click yunica
+
+document.getElementById("yunica").addEventListener("click", verYunica);
+
+function verYunica() {
+  elementoHomero = document.getElementById("info-homero");
+  elementoHomero.style.display = 'none';
+  homeroDatos = document.getElementById("homero");
+  homeroDatos.style.background = 'none';
+  
+  elementoLuna = document.getElementById("info-luna");
+  elementoLuna.style.display = 'none';
+  lunaDatos = document.getElementById("luna");
+  lunaDatos.style.background = 'none';
+
+  elementoLobito = document.getElementById("info-lobito");
+  elementoLobito.style.display = 'none';
+  lobitoDatos = document.getElementById("lobito");
+  lobitoDatos.style.background = 'none';
+  
+  elementoYunica = document.getElementById("info-yunica");
+  elementoYunica.style.display = 'inline';
+  yunicaDatos = document.getElementById("yunica");
+  yunicaDatos.style.background = '#def2f1'; 
+
+  elementoLola = document.getElementById("info-lola");
+  elementoLola.style.display = 'none';
+  lolaDatos = document.getElementById("lola");
+  lolaDatos.style.background = 'none'; 
+
+  elementoOli = document.getElementById("info-oli");
+  elementoOli.style.display = 'none';
+  oliDatos = document.getElementById("oli");
+  oliDatos.style.background = 'none'; 
+
+  elementoPelu= document.getElementById("info-pelu");
+  elementoPelu.style.display = 'none';
+  peluDatos = document.getElementById("pelu");
+  peluDatos.style.background = 'none'; 
+}
+
+// evento captura click lola
+
+document.getElementById("lola").addEventListener("click", verLola);
+
+function verLola() {
+  elementoHomero = document.getElementById("info-homero");
+  elementoHomero.style.display = 'none';
+  homeroDatos = document.getElementById("homero");
+  homeroDatos.style.background = 'none';
+  
+  elementoLuna = document.getElementById("info-luna");
+  elementoLuna.style.display = 'none';
+  lunaDatos = document.getElementById("luna");
+  lunaDatos.style.background = 'none';
+
+  elementoLobito = document.getElementById("info-lobito");
+  elementoLobito.style.display = 'none';
+  lobitoDatos = document.getElementById("lobito");
+  lobitoDatos.style.background = 'none';
+  
+  elementoYunica = document.getElementById("info-yunica");
+  elementoYunica.style.display = 'none';
+  yunicaDatos = document.getElementById("yunica");
+  yunicaDatos.style.background = 'none'; 
+
+  elementoLola = document.getElementById("info-lola");
+  elementoLola.style.display = 'inline';
+  lolaDatos = document.getElementById("lola");
+  lolaDatos.style.background = '#def2f1'; 
+
+  elementoOli = document.getElementById("info-oli");
+  elementoOli.style.display = 'none';
+  oliDatos = document.getElementById("oli");
+  oliDatos.style.background = 'none'; 
+
+  elementoPelu= document.getElementById("info-pelu");
+  elementoPelu.style.display = 'none';
+  peluDatos = document.getElementById("pelu");
+  peluDatos.style.background = 'none'; 
+}
+
+// evento captura click oli
+
+document.getElementById("oli").addEventListener("click", verOli);
+
+function verOli() {
+  elementoHomero = document.getElementById("info-homero");
+  elementoHomero.style.display = 'none';
+  homeroDatos = document.getElementById("homero");
+  homeroDatos.style.background = 'none';
+  
+  elementoLuna = document.getElementById("info-luna");
+  elementoLuna.style.display = 'none';
+  lunaDatos = document.getElementById("luna");
+  lunaDatos.style.background = 'none';
+
+  elementoLobito = document.getElementById("info-lobito");
+  elementoLobito.style.display = 'none';
+  lobitoDatos = document.getElementById("lobito");
+  lobitoDatos.style.background = 'none';
+  
+  elementoYunica = document.getElementById("info-yunica");
+  elementoYunica.style.display = 'none';
+  yunicaDatos = document.getElementById("yunica");
+  yunicaDatos.style.background = 'none'; 
+
+  elementoLola = document.getElementById("info-lola");
+  elementoLola.style.display = 'none';
+  lolaDatos = document.getElementById("lola");
+  lolaDatos.style.background = 'none'; 
+
+  elementoOli = document.getElementById("info-oli");
+  elementoOli.style.display = 'inline';
+  oliDatos = document.getElementById("oli");
+  oliDatos.style.background = '#def2f1'; 
+
+  elementoPelu= document.getElementById("info-pelu");
+  elementoPelu.style.display = 'none';
+  peluDatos = document.getElementById("pelu");
+  peluDatos.style.background = 'none'; 
+}
+
+// evento captura click pelu
+
+document.getElementById("pelu").addEventListener("click", verPelu);
+
+function verPelu() {
+  elementoHomero = document.getElementById("info-homero");
+  elementoHomero.style.display = 'none';
+  homeroDatos = document.getElementById("homero");
+  homeroDatos.style.background = 'none';
+  
+  elementoLuna = document.getElementById("info-luna");
+  elementoLuna.style.display = 'none';
+  lunaDatos = document.getElementById("luna");
+  lunaDatos.style.background = 'none';
+
+  elementoLobito = document.getElementById("info-lobito");
+  elementoLobito.style.display = 'none';
+  lobitoDatos = document.getElementById("lobito");
+  lobitoDatos.style.background = 'none';
+  
+  elementoYunica = document.getElementById("info-yunica");
+  elementoYunica.style.display = 'none';
+  yunicaDatos = document.getElementById("yunica");
+  yunicaDatos.style.background = 'none'; 
+
+  elementoLola = document.getElementById("info-lola");
+  elementoLola.style.display = 'none';
+  lolaDatos = document.getElementById("lola");
+  lolaDatos.style.background = 'none'; 
+
+  elementoOli = document.getElementById("info-oli");
+  elementoOli.style.display = 'none';
+  oliDatos = document.getElementById("oli");
+  oliDatos.style.background = 'none'; 
+
+  elementoPelu= document.getElementById("info-pelu");
+  elementoPelu.style.display = 'inline';
+  peluDatos = document.getElementById("pelu");
+  peluDatos.style.background = '#def2f1'; 
+}
